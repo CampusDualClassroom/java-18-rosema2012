d76157820b5ddb5ab3e13ae1338d282db1bcdb73
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-28 16:19:21 +0200

    Terminado ejercicio18

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -15,7 +15,7 @@ public class Exercise18 {
 	public static int[] createAndInitializeArray(int num){
 		int [] numeros = new int[num];
 		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = (i);
+			numeros[i] = (i+1);
 		}
 		return numeros;
 	}
