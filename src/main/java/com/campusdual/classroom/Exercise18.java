b3e4185ae2b1ed3c8aafa66b720bc527f99dcbfd
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-28 16:17:19 +0200

    Terminado ejercicio18

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,34 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+import jdk.jshell.execution.Util;
+
+import javax.swing.text.Utilities;
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
-
+		int [] nums = createAndInitializeArray(7);
+		showInlineArray(nums);
 	}
+
+	public static int[] createAndInitializeArray(int num){
+		int [] numeros = new int[num];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (i);
+		}
+		return numeros;
+	}
+
+	public static void showInlineArray(int [] nums){
+		for (int i = 0; i < nums.length; i++) {
+			if(i!=nums.length-1) {
+				System.out.print(nums[i] + " ");
+			}else{
+				System.out.println(nums[i]);
+			}
+		}
+	}
+
+
 }
